7fd7508c5721f420388c4db20df10ea981ca46e0
jorgereyesperez <jorgereyesperez1996@gmail.com>
2025-11-14 09:29:57 +0100

    Update Mascotas.py

--- Mascotas.py
+++ Mascotas.py
@@ -144,6 +144,79 @@ def eliminar_mascota(id: int, db:Session=Depends(get_db)):
     db.commit()
     return mascota
 
+@app.put("/api/actualizar_mascota/{id}", response_model=ActualizarMascota)
+def update(id:int, mascota_dto: ActualizarMascota,db:Session=Depends(get_db)):
+    mascota_dto = db.execute(select(Mascota).where(Mascota.id == id)).scalar_one_or_none()
+    if not mascota_dto:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f"No se ha encontrado la mascota con id {id}"
+        )
+    if not mascota_dto.nombre.strip():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="El nombre no puede estar vacío."
+        )       
+        
+    if not mascota_dto.especie.strip():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="La especie no puede estar vacío."
+        )    
+    
+    if not mascota_dto.raza.strip():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="La raza no puede estar vacía."
+        )
+    if not mascota_dto.fecha_nacimiento.strip():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="La fecha no puede estar vacía."
+        )        
 
+    mascota_dto.nombre= mascota_dto.nombre.strip()
+    mascota_dto.especie= mascota_dto.especie.strip()
+    mascota_dto.raza= mascota_dto.raza.strip()
+    mascota_dto.fecha_nacimiento= mascota_dto.fecha_nacimiento.strip()
+    mascota_dto.chip= mascota_dto.chip
+    
+    db.commit()
+    db.refresh(mascota_dto)
+    return mascota_dto
+
+@app.patch("/api/parchear_mascota", response_model=ParcheMascota)
+def parche(id:int, mascota_dto: ParcheMascota,db:Session=Depends(get_db)):
+    mascota= db.execute(select(Mascota).where(Mascota.id == id)).scalar_one_or_none()
+    
+    if not mascota_dto:
+     #status.HTTP_404_NOT_FOUND
+     raise HTTPException(status_code=404, detail= f"404- Mascota con {id} no encontrada")
+ 
+    if mascota_dto.nombre is not None:
+        if not mascota_dto.nombre.strip():
+         raise HTTPException(status_code=400, detail= "El nombre no puede estar vacío")
+        mascota.nombre = mascota_dto.nombre.strip()
+    
+    if mascota_dto.especie is not None:
+        if not mascota_dto.especie.strip():
+         raise HTTPException(status_code=400, detail= "La especie no puede estar vacía")
+        mascota.especie = mascota_dto.especie.strip()
+    
+    
+    if mascota_dto.raza is not None:
+        if not mascota_dto.raza.strip():
+         raise HTTPException(status_code=400, detail= "La raza no puede estar vacía")
+        mascota.raza = mascota_dto.raza.strip()
+    
+    if mascota_dto.fecha_nacimiento is not None:
+        if not mascota_dto.fecha_nacimiento.strip():
+         raise HTTPException(status_code=400, detail= "La fecha no puede estar vacía")
+        mascota.fecha_nacimiento = mascota_dto.fecha_nacimiento.strip()
+    
+    
+    db.commit()
+    db.refresh(mascota)
+    return mascota
 
 #uvicorn Mascotas:app  --reload
