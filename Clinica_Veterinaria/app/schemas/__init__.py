4d8c66b644bdc3a530e23e34c0360d421b8e0b63
jorgereyesperez <jorgereyesperez1996@gmail.com>
2025-11-24 21:32:01 +0100

    Actualizado __init__.py de schemas con mascotas

---
+++ Clinica_Veterinaria/app/schemas/__init__.py
@@ -0,0 +1,3 @@
+from app.schemas.mascota import RespuestaMascota, CrearMascota, ParcheMascota, ActualizarMascota
+
+__all__=["RespuestaMascota", "CrearMascota", "ParcheMascota", "ActualizarMascota"]
