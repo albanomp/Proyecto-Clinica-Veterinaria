f023e8b856878268480f122dbd052684375b11fb
jorgereyesperez <jorgereyesperez1996@gmail.com>
2025-11-27 09:53:02 +0100

    Actualizado __init__.py de schemas

--- Clinica_Veterinaria/app/schemas/__init__.py
+++ Clinica_Veterinaria/app/schemas/__init__.py
@@ -1,3 +1,9 @@
 from app.schemas.mascota import RespuestaMascota, CrearMascota, ParcheMascota, ActualizarMascota
+from app.schemas.veterinario import RespuestaVeterinario, CrearVeterinario, ParcheVeterinario, ActualizarVeterinario
+from app.schemas.Duenyo import DuenyoResponse, DuenyoCreate, DuenyoUpdate, DuenyoPatch
+from app.schemas.Tratamiento import TipoTratamiento, TratamientoCreate, TratamientoUpdate, TratamientoPatch, TratamientoResponse
 
-__all__=["RespuestaMascota", "CrearMascota", "ParcheMascota", "ActualizarMascota"]
+__all__=["RespuestaMascota", "CrearMascota", "ParcheMascota", "ActualizarMascota", "RespuestaVeterinario", "CrearVeterinario", "ParcheVeterinario",
+        "ActualizarVeterinario", "DuenyoResponse", "DuenyoCreate", "DuenyoUpdate", "DuenyoPatch", "TipoTratamiento", "TratamientoCreate",
+        "TratamientoUpdate", "TratamientoPatch", "TratamientoResponse"]
+
