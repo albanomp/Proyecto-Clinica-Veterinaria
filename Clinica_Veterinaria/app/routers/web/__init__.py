ba9da1b70d016cb8832a20ba9a278ad526e40cd6
jorgereyesperez <jorgereyesperez1996@gmail.com>
2025-11-27 09:46:54 +0100

    Actualizado __init__.py de web

--- Clinica_Veterinaria/app/routers/web/__init__.py
+++ Clinica_Veterinaria/app/routers/web/__init__.py
@@ -6,9 +6,15 @@ Contienen los endpoints que renderizan HTMLs
 from fastapi import APIRouter
 from app.routes.web import home
 from app.routes.web import mascotas
+from app.routes.web import veterinario
+from app.routes.web import duenyo
+from app.routes.web import tratamiento
 
 
 router = APIRouter()
 
 router.include_router(home.router)
 router.include_router(mascotas.router)
+router.include_router(veterinario.router)
+router.include_router(duenyo.router)
+router.include_router(tratamiento.router)
