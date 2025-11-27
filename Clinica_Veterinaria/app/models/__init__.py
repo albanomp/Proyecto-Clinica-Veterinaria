6594a138ba7bd1a97c18170c36f04525e92fa4cb
jorgereyesperez <jorgereyesperez1996@gmail.com>
2025-11-27 09:47:50 +0100

    Actualizado __init__.py de models

--- Clinica_Veterinaria/app/models/__init__.py
+++ Clinica_Veterinaria/app/models/__init__.py
@@ -1,3 +1,7 @@
 from app.models.mascota import Mascota
+from app.models.veterinario import Veterinario
+from app.models.veterinario import Duenyo
+from app.models.veterinario import Tratamiento
 
 __all__= ["Mascota","Veterinario","Duenyo","Tratamiento"]
+
