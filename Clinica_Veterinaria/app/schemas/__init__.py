d2afd60858bea553dabdc9f404fc578fc9cfc529
Marcos <129058792+horingurky@users.noreply.github.com>
2025-12-11 01:58:28 +0100

    Add cita schema imports and update __all__ list

--- Clinica_Veterinaria/app/schemas/__init__.py
+++ Clinica_Veterinaria/app/schemas/__init__.py
@@ -2,8 +2,12 @@ from app.schemas.mascota import RespuestaMascota, CrearMascota, ParcheMascota, A
 from app.schemas.duenyo import DuenyoResponse, DuenyoCreate, DuenyoPatch, DuenyoUpdate
 from app.schemas.tratamiento import TratamientoResponse, TratamientoCreate, TratamientoPatch, TratamientoUpdate
 from app.schemas.veterinario import RespuestaVeterinario, CrearVeterinario, ParcheVeterinario, ActualizarVeterinario
+from app.schemas.cita import CitaResponse, CitaCreate, CitaUpdate, CitaPatch
 
 __all__=["RespuestaMascota", "CrearMascota", "ParcheMascota", "ActualizarMascota", "RespuestaVeterinario", "CrearVeterinario", "ParcheVeterinario",
         "ActualizarVeterinario", "DuenyoResponse", "DuenyoCreate", "DuenyoUpdate", "DuenyoPatch", "TipoTratamiento", "TratamientoCreate",
-        "TratamientoUpdate", "TratamientoPatch", "TratamientoResponse", "CitaResponse", "CitaCreate", "CitaUpdate", "CitaPatch"]
+        "TratamientoUpdate", "TratamientoPatch", "TratamientoResponse", "CitaResponse", "CitaCreate", "CitaUpdate", "CitaPatch"
+        ,"CitaResponse",  "CitaCreate", "CitaUpdate", "CitaPatch"
+        ]
+
 
