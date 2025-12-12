44fc107a53087d3e25f700daacbec01b700cb875
Jorge_R <jorgereyesperez1996@gmail.com>
2025-12-12 11:42:24 +0100

    Actualizado archivo web de veteriarios con métodos create, edit y delete y subido html formulario, además de modificar el archivo list

--- Clinica_Veterinaria/app/routers/web/veterinario.py
+++ Clinica_Veterinaria/app/routers/web/veterinario.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, Depends, Form, HTTPException, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -21,6 +21,135 @@ def lista_empleado(request: Request, db:Session = Depends(get_db)):
        
    )
 
+@router.get("/formulario", response_class=HTMLResponse)
+def show_create_form(request: Request):
+   return templates.TemplateResponse(
+       "veterinarios/form.html",
+       {"request":request}
+   )
+
+@router.post("/formulario",response_class=HTMLResponse)
+def crear_veterinario(
+    request: Request,
+    nombre: str = Form(...),
+    colegiado: str = Form(...),
+    especialidad: str = Form(...),
+    telefono: str = Form(...),
+    db:Session = Depends(get_db)
+):
+    errors=[]
+    form_data={
+        "nombre":nombre,
+        "colegiado":colegiado,
+        "especialidad":especialidad,
+        "telefono":telefono
+    }
+    
+    if not nombre.strip():
+        errors.append("El nombre es obligatorio")
+    if not colegiado.strip():
+        errors.append("El colegiado es obligatorio")
+    if not especialidad.strip():
+        errors.append("La especialidad es obligatoria")
+    if not telefono.strip():
+        errors.append("El teléfono es obligatorio")
+    
+    if errors:
+        return templates.TemplateResponse(
+        "veterinarios/form.html",
+        {"request":request, "veterinario":None, "errors":errors, "form_data":form_data}
+    )
+    try:
+        veterinario= Veterinario(
+            nombre= nombre.strip(),
+            colegiado= colegiado.strip(),
+            especialidad= especialidad.strip(),
+            telefono= telefono.strip(),
+        )
+        db.add(veterinario)
+        db.commit()
+        db.refresh(veterinario)
+        return RedirectResponse(url=f"/veterinarios/{veterinario.id}", status_code=303)
+    except Exception as e:
+        db.rollback()
+        errors.append(f"Error al crear el empleado: {str(e)}")
+        return templates.TemplateResponse(
+        "veterinarios/form.html",
+        {"request":request, "veterinario":None, "errors":errors, "form_data":form_data}
+        ) 
+
+@router.get("/{veterinario_id}/editar", response_class=HTMLResponse)
+def show_edit_form(request: Request, veterinario_id: int, db: Session = Depends(get_db)):
+    veterinario = db.execute(select(Veterinario).where(Veterinario.id == veterinario_id)).scalar_one_or_none()
+
+    if veterinario is None:
+        raise HTTPException(status_code=404, detail="404 - Veterinario no encontrada")
+
+    return templates.TemplateResponse(
+        "veterinarios/form.html",
+        {"request": request, "veterinario": veterinario}
+    )
+
+
+@router.post("/{veterinario_id}/editar", response_class=HTMLResponse)
+def update_empleado(
+    request: Request,
+    veterinario_id: int,
+    nombre: str = Form(...),
+    colegiado: str = Form(...),
+    especialidad: str = Form(...),
+    telefono: str = Form(...),
+    db: Session = Depends(get_db)
+):
+    
+    veterinario = db.execute(select(Veterinario).where(Veterinario.id == veterinario_id)).scalar_one_or_none()
+    if veterinario is None:
+        raise HTTPException(status_code=404, detail="404 - Veterinario no encontrada")
+
+
+    errors = []
+    form_data = {
+        "nombre": nombre,
+        "colegiado": colegiado,
+        "especialidad": especialidad,
+        "telefono": telefono
+    }
+
+    if not nombre.strip():
+        errors.append("El nombre es obligatorio")
+    if not colegiado.strip():
+        errors.append("El colegiado es obligatorio")
+    if not especialidad.strip():
+        errors.append("La especialidad es obligatoria")
+    if not telefono.strip():
+        errors.append("El teléfono es obligatorio")
+    
+
+    if errors:
+        return templates.TemplateResponse(
+            "veterinarios/form.html",
+            {"request": request, "veterinario": None, "errors": errors, "form_data": form_data}
+        )
+
+    try:
+        veterinario.nombre = nombre.strip()
+        veterinario.colegiado = colegiado.strip()
+        veterinario.especialidad = especialidad.strip()
+        veterinario.telefono = telefono.strip()
+
+
+        db.commit()
+        db.refresh(veterinario)
+        return RedirectResponse(url=f"/veterinarios/{veterinario.id}", status_code=303)
+    except Exception as e:
+        db.rollback()
+        errors.append(f"Error al actualizar la mascota: {str(e)}")
+
+        return templates.TemplateResponse(
+            "veterinarios/form.html",
+            {"request": request, "veterinario": veterinario, "errors": errors, "form_data": form_data}
+        )
+
 @router.get("/{veterinario_id}", response_class=HTMLResponse)
 def lista_mascota(veterinario_id: int, request: Request, db:Session = Depends(get_db)):
    veterinario= db.execute(select(Veterinario).where(Veterinario.id == veterinario_id)).scalar_one_or_none()
@@ -31,3 +160,17 @@ def lista_mascota(veterinario_id: int, request: Request, db:Session = Depends(ge
        {"request":request, "veterinario":veterinario}
        
    )
+
+
+@router.post("/{veterinario_id}/eliminar", response_class=HTMLResponse)
+def eliminar_empleado(request: Request, veterinario_id: int, db: Session = Depends(get_db)):
+    veterinario = db.execute(select(Veterinario).where(Veterinario.id == veterinario_id)).scalar_one_or_none()
+    if veterinario is None:
+        raise HTTPException(status_code=404, detail="404 - Veterinario no encontrado")
+    try:
+        db.delete(veterinario)
+        db.commit()
+        return RedirectResponse("/veterinarios", status_code=303)
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail=f"Error al eliminar al empleado: {str(e)}")
