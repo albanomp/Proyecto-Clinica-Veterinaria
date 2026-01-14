22ec9f6e20c057aad934334cd1d0abea9f11d7fb
Jorge_R <jorgereyesperez1996@gmail.com>
2026-01-14 18:39:41 +0100

    Introducido imagen de fondo para pantalla home

--- Clinica_Veterinaria/app/main.py
+++ Clinica_Veterinaria/app/main.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from app.routers.api import router as api_router
 from app.database import init_db
 from app.routers.web import router as web_router
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 
 app= FastAPI(title="Veterinaria",version="1.0.0")
+
+BASE_DIR = Path(__file__).resolve().parent
+
+app.mount("/static", StaticFiles(directory=BASE_DIR/"statics(imagenes)"), name="static")
 
 init_db()
 
